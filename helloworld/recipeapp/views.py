5d50b86f4016a8e0dd59bbd10d97f0ad85d9e965
Shreya51098 <108988689+Shreya51098@users.noreply.github.com>
2022-07-18 19:36:36 +0530

    Add files via upload

--- helloworld/recipeapp/views.py
+++ helloworld/recipeapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 from .models import Recipe
+from django.db.models import Q
 from django.core.paginator import Paginator
 
 def home(request):
@@ -21,23 +22,49 @@ def home(request):
 
     return render(request, 'home.html', context=context)
 
-
 def browse(request):
+    
+    
     #View the browse recipe page
 
     #Get the recipes from the model file, then pass them to the browse recipe HTML template
     recipes = Recipe.objects.all()
-    page = request.GET.get('page', 1)
-    paginator = Paginator(recipes, 6)
-    try:
-        newRecepie = paginator.page(page)
-    except PageNotAnInteger:
-        newRecepie = paginator.page(1)
-    except EmptyPage:
-        newRecepie = paginator.page(paginator.num_pages)
+    paginator = Paginator(recipes,6) #Show 6 recipes per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context2 = {
         'recipes': recipes,
-        'newRecepie':newRecepie
+        'page_obj': page_obj,
     }
+    
+
+
     return render(request, 'browse.html', context=context2)
 
+def search(request):
+    recipes=Recipe.objects.all()
+    query=request.GET.get("q")
+    queryset=Recipe.objects.filter(Q(recipe_name__icontains=query))
+    return render(request,'search.html',{'queryset':queryset})
+    #total=results.count()
+    #paginator=Paginator(results,3)
+    #page=request.GET.get("page")
+    #try:
+     #   results=paginator.page(page)
+    #except PageNotAnInteger:
+     #   results=paginator.page(1)
+    #except EmptyPage:
+     #   results=paginator.page(paginator.num_pages)
+    #context3={
+     #   "topic":topic,
+    #   "page":page,
+     #   "total":total,
+      #  "query":query,
+       # "results":results,
+    #}
+
+    #return render(request,'search.html',context=context3)
+    
+    
+
+	
