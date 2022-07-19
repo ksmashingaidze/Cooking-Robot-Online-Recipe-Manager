6db080f3f48ebe57556bcd081eb783b36b279639
soroushseifpour <66325488+soroushseifpour@users.noreply.github.com>
2022-07-18 20:52:50 -0400

    Update views.py

--- helloworld/recipeapp/views.py
+++ helloworld/recipeapp/views.py
@@ -23,48 +23,25 @@ def home(request):
     return render(request, 'home.html', context=context)
 
 def browse(request):
-    
-    
-    #View the browse recipe page
-
-    #Get the recipes from the model file, then pass them to the browse recipe HTML template
     recipes = Recipe.objects.all()
-    paginator = Paginator(recipes,6) #Show 6 recipes per page
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    query = request.GET.get("name")
+    rate = request.GET.get("rate")
+    time = request.GET.get("time")
+    if query:
+        recipes = Recipe.objects.filter(recipe_name__icontains=query,rating__range=(1,rate),cook_time_min__range=(0,time))
+    if rate and time:
+        recipes = Recipe.objects.filter(rating__range=(1, rate),cook_time_min__range=(0, time))
+    paginator=Paginator(recipes,3)
+    page=request.GET.get("page")
+    try:
+      results=paginator.page(page)
+    except PageNotAnInteger:
+      results=paginator.page(1)
+    except EmptyPage:
+      results=paginator.page(paginator.num_pages)
     context2 = {
-        'recipes': recipes,
-        'page_obj': page_obj,
+        'newRecepie': results,
     }
-    
-
-
     return render(request, 'browse.html', context=context2)
-
-def search(request):
-    recipes=Recipe.objects.all()
-    query=request.GET.get("q")
-    queryset=Recipe.objects.filter(Q(recipe_name__icontains=query))
-    return render(request,'search.html',{'queryset':queryset})
-    #total=results.count()
-    #paginator=Paginator(results,3)
-    #page=request.GET.get("page")
-    #try:
-     #   results=paginator.page(page)
-    #except PageNotAnInteger:
-     #   results=paginator.page(1)
-    #except EmptyPage:
-     #   results=paginator.page(paginator.num_pages)
-    #context3={
-     #   "topic":topic,
-    #   "page":page,
-     #   "total":total,
-      #  "query":query,
-       # "results":results,
-    #}
-
-    #return render(request,'search.html',context=context3)
-    
-    
 
 	
