3c477d2876a61238f29216702f65dd27e8cd0970
ksmashingaidze <44344951+ksmashingaidze@users.noreply.github.com>
2022-07-19 20:28:29 -0400

    Delete helloworld directory

--- helloworld/recipeapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('browse', views.browse, name='browse'),
-    path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup'),
-]
-
