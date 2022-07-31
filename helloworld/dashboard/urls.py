ec93e1ac4a9fd2acf797de94d2c1c94e8b443f2d
ksmashingaidze <44344951+ksmashingaidze@users.noreply.github.com>
2022-07-31 00:40:57 -0400

    Delete helloworld directory

--- helloworld/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('userprofile', views.userprofile, name='userprofile'),
-    path('addrecipe', views.addrecipe, name='addrecipe'),
-]
-
