204dbe2e6947b8616867af2155913c89e27163ef
ksmashingaidze <44344951+ksmashingaidze@users.noreply.github.com>
2022-08-04 09:41:32 -0400

    Add files via upload

---
+++ helloworld/recipeapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('browse', views.browse, name='browse'),
+    path('detail/<int:id>', views.detail, name='detail'),
+    path('send_emails/<int:pk>',
+         views.send_emails, name='send_emails')
+]
